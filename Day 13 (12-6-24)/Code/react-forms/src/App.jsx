e31b71af3434a6c40b126705643dce8af15a8fb9
Sudhanshu Singh <sudhanshu@segwitz.com>
2024-06-13 09:45:55 +0530

    add validation to form again

--- Day 13 (12-6-24)/Code/react-forms/src/App.jsx
+++ Day 13 (12-6-24)/Code/react-forms/src/App.jsx
@@ -1,53 +1,63 @@
 import React from "react";
 import { useForm } from "react-hook-form";
-import { z } from "zod";
 
 const App = () => {
   const {
     register,
     handleSubmit,
     formState: { errors, isSubmitting },
-  } = useForm({});
+  } = useForm();
 
   const onSubmit = async (data) => {
     await new Promise((resolve) => setTimeout(resolve, 1000));
     console.log(data);
   };
+
   return (
-    <div className=" w-full max-w-xs">
+    <div className="w-full grid h-screen place-items-center">
       <form
-        className="gap-2 bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 "
+        className="gap-2 bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
         onSubmit={handleSubmit(onSubmit)}
       >
         <h1>CREATE QUESTIONS</h1>
         <div className="mb-4">
           <h1>Question</h1>
           <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            className="shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
             id="question"
             type="text"
-            placeholder="enter question here"
-            {...register("question")}
+            placeholder="Enter question here"
+            {...register("question", {
+              required: "This field is required",
+              validate: (value) => {
+                if (!value.includes("?")) {
+                  return "Question must end with a question mark (?)";
+                }
+                return true;
+              },
+            })}
           />
-          {errors.email && (
-            <div className="text-red-500">{errors.email.message}</div>
+          {errors.question && (
+            <div className="text-red-500">{errors.question.message}</div>
           )}
         </div>
         <div className="mb-6">
           <h1>Weightage</h1>
           <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+            className="shadow appearance-none border rounded w-full py-2 px-3 mb-3 leading-tight focus:outline-none focus:shadow-outline"
             id="marks"
             type="text"
-            placeholder="enter weightage here"
-            {...register("marks")}
+            placeholder="Enter weightage here"
+            {...register("marks", {
+              required: "This field is required.",
+            })}
           />
-          {errors.password && (
+          {errors.marks && (
             <div className="text-red-500">{errors.marks.message}</div>
           )}
         </div>
         <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
           type="submit"
           disabled={isSubmitting}
         >
