b4cc456e143bcbdead11c2c16278a9a65691b03f
Sudhanshu Singh <sudhanshu@segwitz.com>
2024-07-10 09:06:39 +0530

    new work added

--- Day 26 (2-7-24)/youtube_clone/src/components/Videos.jsx
+++ Day 26 (2-7-24)/youtube_clone/src/components/Videos.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Videos = () => {
-  return <div></div>;
+  return <div>videos</div>;
 };
 
 export default Videos;
---
+++ Day 29 (9-7-24)/Code/hangman/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Figure from "./components/Figure";
+import Header from "./components/Header";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Figure />
+    </div>
+  );
+};
+
+export default App;
